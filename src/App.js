0e9bfd3c43fbfab2f21fe6caaf8cb5468e08b6e9
eshabaweja <bawejaesha02@gmail.com>
2022-02-14 18:30:58 +0530

    basic view

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Stack from './components/Stack'
+import Keyboard from './components/Keyboard'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Stack />
+      <Keyboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <div className="Hero">
+            <div className="level"><h2>3</h2>level</div>
+            <div className="score"><h2>463</h2>score</div>
+            <div className="multiplier"><h2>4</h2>multiplier</div>
+        </div>
+    )
+}
